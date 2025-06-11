d5c6d3172183f63b0a8d622c1c37d2dba3487d32
Vadim Niedental <vadim.niedental@gmail.com>
2025-06-11 23:57:39 +0200

    Update versions and file structure.

--- Wrath Of The Fallen Queen/script/miscellaneous/player.js
+++ Wrath Of The Fallen Queen/script/miscellaneous/player.js
@@ -115,6 +115,7 @@ class Player{
     }
     
     playheartSound() {
+
         const heartSound =  new Audio("../sounds/heart.mp3");
         heartSound.addEventListener("canplay", () => {
             heartSound.play();
